74e4011ce8c31b70e920e91d99fe230aade9bcd8
olih <olih@users.noreply.github.com>
2017-03-29 21:16:19 +0100

    Updates deps

--- test/index.js
+++ test/index.js
@@ -1,7 +1,7 @@
-import test from "tape"
-import ntriplesCollection from "../src"
+import test from 'tape';
+import ntriplesCollection from '../src';
 
-test("ntriplesCollection", (t) => {
-  t.plan(1)
-  t.equal(true, ntriplesCollection(), "return true")
-})
+test('ntriplesCollection', (t) => {
+  t.plan(1);
+  t.equal(true, ntriplesCollection(), 'return true');
+});
