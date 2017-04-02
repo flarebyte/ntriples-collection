2e64016aa1ee4bd7114ee698e14ac7a68a501ae3
Olivier Huin <olih@users.noreply.github.com>
2017-04-02 12:28:57 +0100

    List of utility methods for n-triples (#1)
    * adds readNTriplesFile
    * Adds writeNTriplesFile
    * Parses literal values
    * Adds get localized object
    * Adds fallback with alternative languages
    * find objects
    * Create tmp directory if it does not exist
    * Adds default value
    * Updates readme with jsdoc documentation
    * Removes support for node.js 0.12
    * Discard graph property

--- README.md
+++ README.md
@@ -8,6 +8,11 @@
 
 > Utility methods for filtering n-triples
 
+Assumptions:
+* You are using [node.js n3](https://github.com/RubenVerborgh/N3.js)
+* You are filtering small arrays of triples.
+* You are mostly interested in the object value.
+
 ## Install
 
 ```sh
@@ -17,14 +22,148 @@ npm i -D ntriples-collection
 ## Usage
 
 ```js
-import ntriplesCollection from "ntriples-collection"
-
-ntriplesCollection() // true
+import { readNTriplesFile,
+   writeNTriplesFile,
+   findObjectByPredicate,
+   findLocalizedObjectByPredicate,
+   findObjectsByPredicate,
+  } from "ntriples-collection"
 ```
+### Functions
+
+<dl>
+<dt><a href="#readNTriplesFile">readNTriplesFile(filename, function)</a></dt>
+<dd><p>Reads a n-triples file and converts it to an array of triples</p>
+</dd>
+<dt><a href="#writeNTriplesFile">writeNTriplesFile(filename, triples, function)</a></dt>
+<dd><p>Saves an array of triples in a n-triples file</p>
+</dd>
+<dt><a href="#findObjectByPredicate">findObjectByPredicate(triples, predicate, defaultValue)</a> ⇒ <code>string</code></dt>
+<dd><p>Finds the first object value based on the predicate</p>
+</dd>
+<dt><a href="#findLocalizedObjectByPredicate">findLocalizedObjectByPredicate(triples, predicate, language, altLangs, defaultValue)</a> ⇒ <code>string</code></dt>
+<dd><p>Finds the first object value based on the predicate and the language</p>
+</dd>
+<dt><a href="#findObjectsByPredicate">findObjectsByPredicate(triples, predicate)</a> ⇒ <code>array</code></dt>
+<dd><p>Finds all object values based on the predicate</p>
+</dd>
+</dl>
+
+<a name="readNTriplesFile"></a>
+
+### readNTriplesFile(filename, function)
+Reads a n-triples file and converts it to an array of triples
+
+**Kind**: global function  
+
+| Param | Type | Description |
+| --- | --- | --- |
+| filename | <code>string</code> | the n-triples filename |
+| function | <code>callback</code> | callback |
+
+**Example**  
+```js
+// [
+{ graph: "",
+object: '"Flower corp"@en',
+predicate: 'http://purl.org/dc/elements/1.1/publisher',
+subject: 'http://www.site.org/version/123/'
+},
+]
+readNTriplesFile('flowers.nt', (err, triples) => {
+ console.log(triples);
+});
+```
+<a name="writeNTriplesFile"></a>
+
+### writeNTriplesFile(filename, triples, function)
+Saves an array of triples in a n-triples file
+
+**Kind**: global function  
+
+| Param | Type | Description |
+| --- | --- | --- |
+| filename | <code>string</code> | the n-triples filename |
+| triples | <code>array</code> | an array of triples objects |
+| function | <code>callback</code> | callback |
+
+**Example**  
+```js
+// {count: 1}
+ const triples = [
+{ graph: "",
+object: '"Flower corp"@en',
+predicate: 'http://purl.org/dc/elements/1.1/publisher',
+subject: 'http://www.site.org/version/123/'
+},
+]
+writeNTriplesFile('flowers.nt', triples, (err, triples) => {
+ console.log(triples);
+});
+```
+<a name="findObjectByPredicate"></a>
+
+### findObjectByPredicate(triples, predicate, defaultValue) ⇒ <code>string</code>
+Finds the first object value based on the predicate
+
+**Kind**: global function  
+**Returns**: <code>string</code> - the string, integer, float, boolean, moment representing the literal value  
+
+| Param | Type | Description |
+| --- | --- | --- |
+| triples | <code>array</code> | an array of triples objects (subject is ignored) |
+| predicate | <code>string</code> | the uri representing the predicate |
+| defaultValue | <code>object</code> | the object/string to return if null |
+
+**Example**  
+```js
+// returns Amadeus
+findObjectByPredicate(triples, 'http://purl.org/dc/elements/1.1/creator')
+```
+<a name="findLocalizedObjectByPredicate"></a>
+
+### findLocalizedObjectByPredicate(triples, predicate, language, altLangs, defaultValue) ⇒ <code>string</code>
+Finds the first object value based on the predicate and the language
+
+**Kind**: global function  
+**Returns**: <code>string</code> - the string, integer, float, boolean, moment representing the literal value  
+
+| Param | Type | Description |
+| --- | --- | --- |
+| triples | <code>array</code> | an array of triples objects (subject is ignored) |
+| predicate | <code>string</code> | the uri representing the predicate |
+| language | <code>string</code> | the requested language |
+| altLangs | <code>array</code> | an array of alternative languages (max 2) |
+| defaultValue | <code>object</code> | the object/string to return if null |
+
+**Example**  
+```js
+// returns Amadeus
+findLocalizedObjectByPredicate(triples, 'http://purl.org/dc/elements/1.1/creator', 'fr', ['en'])
+```
+<a name="findObjectsByPredicate"></a>
+
+### findObjectsByPredicate(triples, predicate) ⇒ <code>array</code>
+Finds all object values based on the predicate
+
+**Kind**: global function  
+**Returns**: <code>array</code> - of string, integer, float, boolean, moment representing the literal values  
+
+| Param | Type | Description |
+| --- | --- | --- |
+| triples | <code>array</code> | an array of triples objects (subject is ignored) |
+| predicate | <code>string</code> | the uri representing the predicate |
+
+**Example**  
+```js
+// returns [Amadeus, Bach]
+findObjectsByPredicate(triples, 'http://purl.org/dc/elements/1.1/creator')
+```
+
 
 ## License
 
-MIT © [olih](http://github.com/flarebyte)
+MIT © [Olivier Huin](http://github.com/flarebyte)
 
 [npm-url]: https://npmjs.org/package/ntriples-collection
 [npm-image]: https://img.shields.io/npm/v/ntriples-collection.svg?style=flat-square
--- src/index.js
+++ src/index.js
@@ -1,7 +1,174 @@
+import _ from 'lodash';
+import _S from 'string';
+import moment from 'moment';
+import fs from 'fs';
+import { Util, Parser, Writer } from 'n3';
+
+const isTriple = str => _S(str).contains('<');
+
+const omitGraph = t => _.pick(t, ['subject', 'predicate', 'object']);
+const withoutGraph = triples => _.map(triples, omitGraph);
+
+const normalizeValue = (value, defaultValue) => {
+  if (_.isNil(value)) {
+    return defaultValue;
+  }
+  if (Util.isLiteral(value)) {
+    const litType = Util.getLiteralType(value);
+    const litValue = Util.getLiteralValue(value);
+    switch (litType) {
+      case 'http://www.w3.org/2001/XMLSchema#string': return litValue;
+      case 'http://www.w3.org/2001/XMLSchema#anyURI': return litValue;
+      case 'http://www.w3.org/2001/XMLSchema#integer': return _S(litValue).toInt();
+      case 'http://www.w3.org/2001/XMLSchema#nonPositiveInteger': return _S(litValue).toInt();
+      case 'http://www.w3.org/2001/XMLSchema#negativeInteger': return _S(litValue).toInt();
+      case 'http://www.w3.org/2001/XMLSchema#nonNegativeInteger': return _S(litValue).toInt();
+      case 'http://www.w3.org/2001/XMLSchema#positiveInteger': return _S(litValue).toInt();
+      case 'http://www.w3.org/2001/XMLSchema#float': return _S(litValue).toFloat();
+      case 'http://www.w3.org/2001/XMLSchema#boolean': return _S(litValue).toBool();
+      case 'http://www.w3.org/2001/XMLSchema#dateTime': return moment(litValue, moment.ISO_8601);
+      case 'http://www.w3.org/2001/XMLSchema#date': return moment(litValue, moment.ISO_8601);
+      case 'http://www.w3.org/2001/XMLSchema#time': return litValue;
+      default: return litValue;
+    }
+  }
+  if (Util.isIRI(value)) {
+    return value;
+  }
+  return value;
+};
+
+const normalizeValues = (values, defaultValue) =>
+  _.map(values, v => normalizeValue(v, defaultValue));
+
+const asSemanticValue = (value) => {
+  const literalValue = normalizeValue(value, null);
+  const literalType = Util.getLiteralType(value);
+  const literalLanguage = Util.getLiteralLanguage(value);
+  return { value, literalValue, literalType, literalLanguage };
+};
+
+const asSemanticValues = values => _.map(values, asSemanticValue);
+
 /**
- * @param {Type}
- * @return {Type}
+ * Reads a n-triples file and converts it to an array of triples
+ * @param {string} filename - the n-triples filename
+ * @param {callback} function - callback
+ * @example
+ * // [
+ * { graph: "",
+ * object: '"Flower corp"@en',
+ * predicate: 'http://purl.org/dc/elements/1.1/publisher',
+ * subject: 'http://www.site.org/version/123/'
+ * },
+ * ]
+ * readNTriplesFile('flowers.nt', (err, triples) => {
+ *  console.log(triples);
+ * });
  */
-export default function () {
-  return true;
+export function readNTriplesFile(filename, callback) {
+  const n3parser = new Parser();
+  const n3parse = str => _.head(n3parser.parse(str));
+
+  fs.readFile(filename, 'utf8', (err, data) => {
+    if (err) {
+      callback(err);
+    } else {
+      const lines = _.filter(_S(data).lines(), isTriple);
+      const triples = _.map(lines, n3parse);
+      callback(null, withoutGraph(triples));
+    }
+  });
+}
+
+/**
+ * Saves an array of triples in a n-triples file
+ * @param {string} filename - the n-triples filename
+ * @param {array} triples - an array of triples objects
+ * @param {callback} function - callback
+ * @example
+ * // {count: 1}
+ *  const triples = [
+ * { graph: "",
+ * object: '"Flower corp"@en',
+ * predicate: 'http://purl.org/dc/elements/1.1/publisher',
+ * subject: 'http://www.site.org/version/123/'
+ * },
+ * ]
+ * writeNTriplesFile('flowers.nt', triples, (err, triples) => {
+ *  console.log(triples);
+ * });
+ */
+export function writeNTriplesFile(filename, triples, callback) {
+  const fstream = fs.createWriteStream(filename);
+  const n3writer = new Writer(fstream, { format: 'N-Triples' });
+  n3writer.addTriples(withoutGraph(triples));
+  n3writer.end((error) => {
+    const count = _.size(triples);
+    const e = _.isNil(error) ? null : { error, filename };
+    callback(e, { count });
+  });
+}
+
+/**
+ * Finds the first object value based on the predicate
+ * @param {array} triples - an array of triples objects (subject is ignored)
+ * @param {string} predicate - the uri representing the predicate
+ * @param {object} defaultValue - the object/string to return if null
+ * @example
+ * // returns Amadeus
+ * findObjectByPredicate(triples, 'http://purl.org/dc/elements/1.1/creator')
+ * @return {string} the string, integer, float, boolean, moment representing the literal value
+ */
+export function findObjectByPredicate(triples, predicate, defaultValue = null) {
+  const value = _.get(_.find(triples, { predicate }), 'object');
+  return normalizeValue(value, defaultValue);
+}
+
+/**
+ * Finds the first object value based on the predicate and the language
+ * @param {array} triples - an array of triples objects (subject is ignored)
+ * @param {string} predicate - the uri representing the predicate
+ * @param {string} language - the requested language
+ * @param {array} altLangs - an array of alternative languages (max 2)
+ * @param {object} defaultValue - the object/string to return if null
+ * @example
+ * // returns Amadeus
+ * findLocalizedObjectByPredicate(triples, 'http://purl.org/dc/elements/1.1/creator', 'fr', ['en'])
+ * @return {string} the string, integer, float, boolean, moment representing the literal value
+ */
+export function findLocalizedObjectByPredicate(triples, predicate, language, altLangs,
+  defaultValue = null) {
+  const values = _.map(_.filter(triples, { predicate }), 'object');
+  if (_.isEmpty(values)) {
+    return defaultValue;
+  }
+
+  const semanticValues = asSemanticValues(values);
+  const altLang = _.nth(altLangs);
+  const altLang2 = _.nth(altLangs, 1);
+  const value = _.find(semanticValues, { literalLanguage: language });
+  if (_.isNil(altLang) || !_.isNil(value)) {
+    return _.get(value, 'literalValue', defaultValue);
+  }
+  const altValue = _.find(semanticValues, { literalLanguage: altLang });
+  if (_.isNil(altLang2) || !_.isNil(altValue)) {
+    return _.get(altValue, 'literalValue', defaultValue);
+  }
+  const altValue2 = _.find(semanticValues, { literalLanguage: altLang2 });
+  return _.get(altValue2, 'literalValue', defaultValue);
+}
+
+/**
+ * Finds all object values based on the predicate
+ * @param {array} triples - an array of triples objects (subject is ignored)
+ * @param {string} predicate - the uri representing the predicate
+ * @example
+ * // returns [Amadeus, Bach]
+ * findObjectsByPredicate(triples, 'http://purl.org/dc/elements/1.1/creator')
+ * @return {array} of string, integer, float, boolean, moment representing the literal values
+ */
+export function findObjectsByPredicate(triples, predicate) {
+  const values = _.map(_.filter(triples, { predicate }), 'object');
+  return normalizeValues(values);
 }
